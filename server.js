5c4afb0e3e05adf7f017b6b3b520f08398323ea4
The Collective <148755759+Pl-x@users.noreply.github.com>
2025-05-15 19:41:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,15 +16,9 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = process.env.NODE_ENV === 'production'
-  ? ['https://yourdomain.com']
-  : ['http://127.0.0.1:3000'];
 
 // Middleware
-app.use(cors({
-    origin: allowedOrigins,
-    credentials: true
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(helmet({
   frameguard: { action: 'deny' }
